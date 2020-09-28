ddc74c81a9fe16331079ace70b745f0495f4c13b
Danil <shihalevdanil@icloud.com>
2020-09-28 22:55:46 +0500

    Add":"

--- src/com/company/WordCounter.java
+++ src/com/company/WordCounter.java
@@ -6,7 +6,7 @@ public class WordCounter {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите текст а я посчитаю сколько слов");
+        System.out.println("Введите текст а я посчитаю сколько слов:");
         String userString = scanner.nextLine();
         String[] words = userString.split(" ");
         System.out.println(words.length);
