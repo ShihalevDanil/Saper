e667b007462ce98420af8de7dc3602fb5956634b
Danil <shihalevdanil@icloud.com>
2020-10-02 17:32:23 +0500

    isNuber

--- src/com/company/WordCounter.java
+++ src/com/company/WordCounter.java
@@ -10,5 +10,30 @@ public class WordCounter {
         String userString = scanner.nextLine();
         String[] words = userString.split(" ");
         System.out.println(words.length);
+        int numberCounter = 0;
+        for(int i = 0; i < words.length; i++) {
+            if(isNumber(words[i])) {
+                numberCounter++;
+            }
+        }
+        System.out.println("Количество чисел в строке" + numberCounter);
+    }
+
+    public static boolean isNumber(String word) {
+        char[] chars = word.toCharArray();
+        for (char i = 0; i < chars.length; i++){
+            if(Character.isDigit(chars[i])){
+                return true;
+            }
+        }
+        /*word.toCharArray()
+        for (char i = 0; i++) {
+            if(Character.isDigit(i))
+                return true;
+
+
+        }
+        return false;*/
+        return false;
     }
 }
